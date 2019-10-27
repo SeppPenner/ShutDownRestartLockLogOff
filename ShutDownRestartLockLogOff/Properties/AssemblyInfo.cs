f47ef02260b8f69cf38198087576f51be7fa8714
SeppPenner <HansM2013@gmx.de>
2019-10-27 21:31:00 +0100

    Updated nuget packages, added GitVersionTask.

--- ShutDownRestartLockLogOff/Properties/AssemblyInfo.cs
+++ ShutDownRestartLockLogOff/Properties/AssemblyInfo.cs
@@ -20,16 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a8b6cf52-1c7d-45cd-8102-a93232532f84")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
