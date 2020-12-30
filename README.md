ff5c9da9c1a1aad044d2fa3e26c2a8b08c6486f7
SeppPenner <HansM2013@gmx.de>
2020-12-30 20:30:16 +0100

    Removed license year, moved changelog to extra file, adjusted folder structure, updated nuget packages, moved to Net 5.0.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ ShutDownRestartLockLogOff
 ====================================
 
 ShutDownRestartLockLogOff is an executable to check how to shutdown, restart, lock or log off from Windows.
-The executable was written and tested in .Net 4.8.
+The executable was written and tested in .Net 5.0.
 
 [![Build status](https://ci.appveyor.com/api/projects/status/d4kk3i97489ni27l?svg=true)](https://ci.appveyor.com/project/SeppPenner/shutdownrestartlocklogoff)
 [![GitHub issues](https://img.shields.io/github/issues/SeppPenner/ShutDownRestartLockLogOff.svg)](https://github.com/SeppPenner/ShutDownRestartLockLogOff/issues)
@@ -11,10 +11,7 @@ The executable was written and tested in .Net 4.8.
 [![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://raw.githubusercontent.com/SeppPenner/ShutDownRestartLockLogOff/master/License.txt)
 [![Known Vulnerabilities](https://snyk.io/test/github/SeppPenner/ShutDownRestartLockLogOff/badge.svg)](https://snyk.io/test/github/SeppPenner/ShutDownRestartLockLogOff)
 
-
 Change history
 --------------
 
-* **Version 1.0.1.0 (2019-10-27)** : Updated nuget packages, added GitVersionTask.
-* **Version 1.0.0.1 (2019-05-05)** : Updated .Net version to 4.8.
-* **Version 1.0.0.0 (2017-03-20)** : 1.0 release.
+See the [Changelog](https://github.com/SeppPenner/ShutDownRestartLockLogOff/blob/master/Changelog.md).
---
+++ src/ShutDownRestartLockLogOff/Main.cs
@@ -0,0 +1,87 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Main.cs" company="Hämmer Electronics">
+//   Copyright (c) All rights reserved.
+// </copyright>
+// <summary>
+//   The main form.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ShutDownRestartLockLogOff
+{
+    using System;
+    using System.Diagnostics;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Runtime.InteropServices;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// The main form.
+    /// </summary>
+    public partial class Main : Form
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Main"/> class.
+        /// </summary>
+        public Main()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Locks the work station.
+        /// </summary>
+        [DllImport("user32")]
+        private static extern void LockWorkStation();
+
+        /// <summary>
+        /// Exits the window exceptions.
+        /// </summary>
+        /// <param name="uFlags">The flags.</param>
+        /// <param name="dwReason">The reason.</param>
+        /// <returns>A <see cref="bool"/> value indicating whether the exit process worked or not.</returns>
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1305:FieldNamesMustNotUseHungarianNotation", Justification = "Reviewed. Suppression is OK here.")]
+        [DllImport("user32")]
+        private static extern bool ExitWindowsEx(uint uFlags, uint dwReason);
+
+        /// <summary>
+        /// Handles the shutdown button click.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The event args.</param>
+        private void ShutdownButtonClick(object sender, EventArgs e)
+        {
+            Process.Start("shutdown", "/s /t 0");
+        }
+
+        /// <summary>
+        /// Handles the restart button click.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The event args.</param>
+        private void RestartButtonClick(object sender, EventArgs e)
+        {
+            Process.Start("shutdown", "/r /t 0");
+        }
+
+        /// <summary>
+        /// Handles the log off button click.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The event args.</param>
+        private void LogOffButtonClick(object sender, EventArgs e)
+        {
+            ExitWindowsEx(0, 0);
+        }
+
+        /// <summary>
+        /// Handles the lock button click.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The event args.</param>
+        private void LockButtonClick(object sender, EventArgs e)
+        {
+            LockWorkStation();
+        }
+    }
+}
