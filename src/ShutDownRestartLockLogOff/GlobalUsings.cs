d15c88cae3c729d6cdf42e88b57d9ad129bd51d4
SeppPenner <HansM2013@gmx.de>
2022-02-17 08:58:52 +0100

    Added nullable checks, added editorconfig, added file scoped namespaces, added global usings.

---
+++ src/ShutDownRestartLockLogOff/GlobalUsings.cs
@@ -0,0 +1,4 @@
+#pragma warning disable IDE0065 // Die using-Anweisung wurde falsch platziert.
+global using System.Diagnostics;
+global using System.Runtime.InteropServices;
+#pragma warning restore IDE0065 // Die using-Anweisung wurde falsch platziert.
